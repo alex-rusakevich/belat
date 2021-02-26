edf3fe4aa96a851c5e8671e2e8ddd3ee6f0e1b4d
Alex Rusakevich <mr.alexander.rusakevich@gmail.com>
2021-02-26 22:55:35 +0300

    Fixed г in classic

--- belat/classic.py
+++ belat/classic.py
@@ -21,7 +21,7 @@ class Scheme(bs.Scheme):
     c_vialik_lit = "БВГДЖЗЙКЛМНОПРСТУЎФХЦЧШАЕОІУЫЯЮ"
     c_mal_lit = "бвгджзйклмнпрстфхцчшаеоіуыяю"
 
-    gram_baza_je = {"l_galosn_lit":l_galosn_lit, "l_zychn_lit":l_zychn_lit, 
+    gram_baza = {"l_galosn_lit":l_galosn_lit, "l_zychn_lit":l_zychn_lit, 
                    "l_vialik_lit":l_vialik_lit, "l_mal_lit":l_mal_lit,
 
                    "c_galosn_lit":c_galosn_lit, "c_zychn_lit":c_zychn_lit, 
@@ -29,29 +29,40 @@ class Scheme(bs.Scheme):
     
     # Праца з літарай г і Г
     # У некаторых каранях пішацца g(выбухны гук) замест h(фрыкатыўны)
-    # TODO Зрабіць для такіх опцый як няГеглы, нягеГлы і г.д.
     class ctlr_g(bs.Rule):
         # @ у каранях - г або Г
         karani = ["@узік","@анак","@онт",
                 "@узіч","@анач","@онц",
-                "@анк", "ня@е@л"]
+                "@анк", "ня@е@л", "маз@і"]
 
         def work_with(self, text, regexp_rule):
+            result = text
+
+            for i in self.karani:
+                cur_state = 0
+                while re.match(i.replace("@","г"), result[cur_state:], re.IGNORECASE):
+                    cur_state = re.search(i.replace("@","г"), result[cur_state:], re.IGNORECASE).start()
+                    # Параўноўваем
+                    # @узік
+                    # ГуЗіК
+                    # GуЗіК
+                    try:
+                        count = 0
+                        while not re.match("\s",result[cur_state]):
+                            if i[count] == "@":
+                                if result[cur_state] == "Г":
+                                    result = result[:cur_state]+"G"+result[(cur_state+1):]
+                                elif result[cur_state] == "г":
+                                    result = result[:cur_state]+"g"+result[(cur_state+1):]
+                            count += 1
+                            cur_state +=1
+                    except IndexError:
+                        continue
+
             if regexp_rule == "г":
-                result = text
-                for i in self.karani:
-                    result = re.sub(i.replace("@","г"), i.replace("@","g"),
-                        result)
                 return re.sub("г", "h", result)
 
             elif regexp_rule == "Г":
-                result = text
-                for i in self.karani:
-                    result = re.sub(i.replace("@","Г"), i.replace("@","G"),
-                        result)
-                    i = i.upper()
-                    result = re.sub(i.replace("@","Г"), i.replace("@","G"),
-                        result)
                 return re.sub("Г", "H", result)
 
     # Праца з літарамі Е, Ю, Ё, Я, е, ю, ё, я
@@ -59,7 +70,7 @@ class Scheme(bs.Scheme):
     # Je je - у пачатку слова, пасля галосных, пасля апострафа, пасля ў
     # Ie ie - пасля зычных, акрамя ў і л
     # E e - пасля л
-    class ctr_je_jo_ju_ja(bs.Rule):
+    class ctlr_je_jo_ju_ja(bs.Rule):
         def re_find(what, where, start):
             return re.search(what, where, )
 
@@ -115,60 +126,87 @@ class Scheme(bs.Scheme):
                         continue
                 return e_res
 
-    class ctr_l(bs.Rule):
-        def work_with(self, text, regexp):
-            pass
+    # Выбар паміж Ł і L
+    class ctlr_l(bs.Rule):
+        def work_with(self, text, regexp_rule):
+            excepts = "еёіюяЕЁІЮЯ"
+            if regexp_rule == "л":
+                li = re.sub("л(?=["+excepts+"])", "l", text) 
+                return re.sub("л(?!["+excepts+"])", "ł", li)
+            elif regexp_rule == "Л":
+                li = re.sub("Л(?=["+excepts+"])", "L", text) 
+                return re.sub("Л(?!["+excepts+"])", "Ł", li)
 
-    class ctr_z(bs.Rule):
-        def work_with(self, text, regexp):
-            pass
+    # Выбар паміж CH і Ch, ch
+    class ctrl_ch(bs.Rule):
+        def __init__(self, gram_baza):
+            self.gram_baza = gram_baza
 
-    class ctr_s(bs.Rule):
-        def work_with(self, text, regexp):
-            pass
+        def work_with(self, text, regexp_rule):
+            if regexp_rule == "х":
+                return re.sub("х", "ch", text)
+            elif regexp_rule == "Х":
+                ch_text = re.sub("Х", "Ch", text)
 
-    class ctr_c(bs.Rule):
-        def work_with(self, text, regexp):
+                cur_st = 0
+                vialik_lit = "["+self.gram_baza["l_vialik_lit"]+self.gram_baza["c_vialik_lit"]+"]"
+                while "Ch" in ch_text[cur_st:]:
+                    cur_st = ch_text.index("Ch", cur_st)
+                    try:
+                        if re.match(vialik_lit,ch_text[cur_st-1]) or re.match(vialik_lit, ch_text[cur_st+2]):
+                            ch_text = ch_text[:cur_st+1] + "H" + ch_text[cur_st+2:]
+                    except IndexError:
+                        continue
+                return ch_text
+
+    class assimil_pa_miahk(bs.Rule):
+        def __init__(self, gram_baza):
+            self.gram_baza = gram_baza
+        
+        def work_with(self, text, regexp_rule):
             pass
 
     ctl_rules = {
-        "г":ctlr_g(), "Г":ctlr_g(), 
-        "Е":ctr_je_jo_ju_ja(gram_baza_je), "е":ctr_je_jo_ju_ja(gram_baza_je),
+        "г":ctlr_g(), "Г":ctlr_g(),
+        "Л":ctlr_l(), "л":ctlr_l(),
+        "Х":ctrl_ch(gram_baza), "х":ctrl_ch(gram_baza),
+        "Е":ctlr_je_jo_ju_ja(gram_baza), "е":ctlr_je_jo_ju_ja(gram_baza),
+        "Ё":ctlr_je_jo_ju_ja(gram_baza), "ё":ctlr_je_jo_ju_ja(gram_baza),
+        "Ю":ctlr_je_jo_ju_ja(gram_baza), "ю":ctlr_je_jo_ju_ja(gram_baza),
+        "Я":ctlr_je_jo_ju_ja(gram_baza), "я":ctlr_je_jo_ju_ja(gram_baza),
+        
+        "З":"Z", "з":"z",
+        "Н":"N", "н":"n",
+        "С":"S", "с":"s",
+        "Ц":"C", "ц":"c",
+        
         "Зь|ЗЬ":"Ź", "зь":"ź",
         "Ль|ЛЬ":"L", "ль":"l",
         "Нь|НЬ":"Ń", "нь":"ń",
         "Сь|СЬ":"Ś", "сь":"ś",
         "Ць|ЦЬ":"Ć", "ць":"ć",
+
         "А":"A", "а":"a",
         "Б":"B", "б":"b",
         "В":"V", "в":"v",
         "Д":"D", "д":"d",
-        "Ё":ctr_je_jo_ju_ja(gram_baza_je), "ё":ctr_je_jo_ju_ja(gram_baza_je),
         "Ж":"Ž", "ж":"ž",
-        "З":ctr_z, "з":ctr_z, # TODO
         "І":"I", "і":"i",
         "Й":"J", "й":"j",
         "К":"K", "к":"k",
-        "Л":ctr_l, "л":ctr_l, # TODO
         "М":"M", "м":"m",
-        "Н":"N", "н":"n",
         "О":"O", "о":"o",
         "П":"P", "п":"p",
         "Р":"R", "р":"r",
-        "С":ctr_s, "с":ctr_s, # TODO
         "Т":"T", "т":"t",
         "У":"U", "у":"u",
         "Ў":"Ŭ", "ў":"ŭ",
         "Ф":"F", "ф":"f",
-        "Х":"Ch", "х":"ch",
-        "Ц":ctr_c, "ц":ctr_c, # TODO
         "Ч":"Č", "ч":"č",
         "Ш":"Š", "ш":"š",
         "Ы":"Y", "ы":"y",
         "Ь":"\u0301", "ь":"\u0301",
         "Э":"E", "э":"e",
-        "Ю":ctr_je_jo_ju_ja(gram_baza_je), "ю":ctr_je_jo_ju_ja(gram_baza_je),
-        "Я":ctr_je_jo_ju_ja(gram_baza_je), "я":ctr_je_jo_ju_ja(gram_baza_je),
         "Ґ":"G", "ґ":"g"
     }
 
