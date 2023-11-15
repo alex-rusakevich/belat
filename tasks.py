13801b6c52fd6bf29321e934530842ccb6d16f34
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-11-15 21:36:10 +0300

    #patch automatic version bumper

--- tasks.py
+++ tasks.py
@@ -1,10 +1,9 @@
 import os
+from curses.ascii import BEL
+from io import StringIO
+from sys import stdout
 
 from invoke import run, task
-
-BELAT_VERSION = (
-    open(os.path.join("belat", "VERSION.txt"), "r", encoding="utf8").read().strip()
-)
 
 os.environ["BELAT_BASE_DIR"] = ".belat"
 
@@ -51,7 +50,51 @@ def designer(context):
 
 
 @task
+def update_version_txt(context):
+    BELAT_VERSION = (
+        open(os.path.join("belat", "VERSION.txt"), "r", encoding="utf8").read().strip()
+    )
+
+    latest_commit_msg = StringIO()
+    run("git log -1 --pretty=%B", out_stream=latest_commit_msg)
+    latest_commit_msg = latest_commit_msg.getvalue().strip()
+
+    major, minor, patch = (int(i) for i in BELAT_VERSION.split("."))
+
+    if "#patch" in latest_commit_msg:
+        patch += 1
+    elif "#minor" in latest_commit_msg:
+        patch = 0
+        minor += 1
+    elif "#major" in latest_commit_msg:
+        patch, minor = 0, 0
+        major += 1
+
+    NEW_VER = ".".join([str(i) for i in (major, minor, patch)])
+
+    if NEW_VER == BELAT_VERSION:
+        print("No new version marker, skipping")
+    else:
+        open(os.path.join("belat", "VERSION.txt"), "w", encoding="utf8").write(NEW_VER)
+        print(f"New version is {NEW_VER}")
+        run(f'git add -A . && git commit -m "Bumped version to {NEW_VER}"')
+
+
+@task(pre=(update_version_txt,))
 def tag(context):
     """Auto add tag to git commit depending on belat version"""
-    run(f"git tag v{BELAT_VERSION}")
-    run(f"git push --tags")
+
+    BELAT_VERSION = (
+        open(os.path.join("belat", "VERSION.txt"), "r", encoding="utf8").read().strip()
+    )
+
+    latest_tag = StringIO()
+
+    run("git describe --abbrev=0 --tags", out_stream=latest_tag)
+    latest_tag = latest_tag.getvalue().strip()
+
+    if f"v{BELAT_VERSION}" != latest_tag:
+        run(f"git tag v{BELAT_VERSION}")
+        run(f"git push --tags")
+    else:
+        print("No new version, skipping")
