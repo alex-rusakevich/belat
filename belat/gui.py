095383534b87112616931e824a9e08c8b334bb05
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-11-15 23:12:42 +0300

    #patch added version to tile

--- belat/gui.py
+++ belat/gui.py
@@ -3,6 +3,8 @@ import sys
 
 from PyQt6 import QtGui, QtWidgets, uic
 
+import belat
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -10,6 +12,7 @@ class MainWindow(QtWidgets.QMainWindow):
         uic.loadUi(os.path.join("ui", "belat.ui"), self)
         self.setContentsMargins(11, 11, 11, 11)
         self.setWindowIcon(QtGui.QIcon(os.path.join("ui", "icon.png")))
+        self.setWindowTitle(f"belat v{belat.__version__}")
 
 
 app = QtWidgets.QApplication(sys.argv)
