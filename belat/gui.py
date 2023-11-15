435a3e9d86c7bd24337b957dca95547ba83e453d
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-11-15 22:56:55 +0300

    #patch updated icon file

--- belat/gui.py
+++ belat/gui.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from PyQt6 import QtWidgets, uic
+from PyQt6 import QtGui, QtWidgets, uic
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -9,6 +9,7 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__(*args, **kwargs)
         uic.loadUi(os.path.join("ui", "belat.ui"), self)
         self.setContentsMargins(11, 11, 11, 11)
+        self.setWindowIcon(QtGui.QIcon(os.path.join("ui", "icon.png")))
 
 
 app = QtWidgets.QApplication(sys.argv)
